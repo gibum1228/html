7f70485f9c8faf95b39db8df2501007467a6402b
김기범 <38284922+gibum1228@users.noreply.github.com>
2020-06-10 01:42:49 +0900

    최종 프로젝트

--- pjt_final/pjt_final.js
+++ pjt_final/pjt_final.js
@@ -4,9 +4,10 @@ imgArray[0] = "image/0.jpg";
 imgArray[1] = "image/1.jpg";
 imgArray[2] = "image/2.jpg";
 imgArray[3] = "image/3.jpg";
+imgArray[4] = "image/4.jpg";
 
 function showImage(){
-    var imgNum = Math.round(Math.random() * 3);
+    var imgNum = Math.round(Math.random() * 4);
     var objImg = document.getElementById("introImg");
     objImg.src = imgArray[imgNum];
     setTimeout(showImage, 5000);
