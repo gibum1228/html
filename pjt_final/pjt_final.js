f9fbb410375414490cde48ad707e5adf2d2329f6
김기범 <38284922+gibum1228@users.noreply.github.com>
2020-06-14 03:33:42 +0900

    최종 프로젝트

--- pjt_final/pjt_final.js
+++ pjt_final/pjt_final.js
@@ -14,70 +14,6 @@ function showImage(){
 }
 
 
-//계산기
-//지우기
-function backward() {
-    var exp = document.getElementById("exp");
-    var len = exp.value.length;
-    if(len == 0)
-      return;
-    else
-      exp.value = exp.value.substr(0, len-1);
-}
-
-//초기화
-function clearexp() {
-    var exp = document.getElementById("exp");
-    exp.value = "0";
-}
-
-//식 추가
-function addInput(value) {
-    var exp = document.getElementById("exp");
-    if(exp.value == "0")
-      exp.value = value;
-    else
-      exp.value += value;
-}
-
-//수식 계산
-function calulate() {
-    var exp = document.getElementById("exp");
-    var str = String(exp.value); // exp.value를 String 형태로 저장하기
-    var strPow = str.split("^");
-    var strLog = str.split("log");
-
-    if(strPow.length == 2){ // 제곱 ex) 50^2 -> {50, 2}
-      exp.value = Math.pow(strPow[0], strPow[1]);
-    }else if(str.charAt(str.length-1) == "!"){ // 팩토리얼 계산
-      var fac = parseInt(str.substring(0, str.length));
-      var result = 1;
-
-      for(var i = 2; i <= fac; i++){
-        result *= i;
-      }
-
-      exp.value = result;
-    }else if(str.charAt(0) == "√") { // 루트 ex) √100
-      str = str.substring(1, str.length); // √를 제외한 숫자 구하기 -> 1부터 str.length - 1 까지
-
-      exp.value = Math.sqrt(str);
-    }else if(str.substring(0, 3) == "sin"){ // sin
-      exp.value = Math.sin(str.substring(3, str.length));
-    }else if(str.substring(0, 3) == "cos"){ // cos
-      exp.value = Math.cos(str.substring(3, str.length));
-    }else if(str.substring(0, 3) == "tan"){ // tan
-      exp.value = Math.tan(str.substring(3, str.length));
-    }else if(str.substring(0, 3) == "log"){ // log10
-      var x = parseInt(str.substring(3, str.length));
-
-      exp.value = Math.log10(x);
-    }else{ // 일반 수식
-      exp.value = eval(exp.value);
-    }
-}
-
-
 // 시계
 window.addEventListener("load", function(){
   clockRun();
@@ -113,4 +49,28 @@ function clockRun() {
   document.getElementById("hour").style.transform = h_angle_value;
 
   setTimeout(clockRun, 1000); // 1초마다 재시작
+}
+
+
+// 방명록 작성
+function addGuestBook() {
+  var guestExp = document.getElementById("guestExp"); // 방명록 글
+  var obj = document.getElementById("guestBookBox"); // 부모
+  var s = String(guestExp.value);
+
+  if(s == "" || s == null || s == " "){
+    guestExp.value = "";
+    alert("내용을 적어주세요.");
+  }else{
+    var child = document.createElement("li");
+
+    child.innerHTML = s;
+    child.addEventListener("click", function(){ // 클릭 이벤트
+      var p = this.parentElement; // 부모 HTML 요소
+      p.removeChild(this); // 부모에서 자식을 삭제
+    });
+
+    guestExp.value = "";
+    obj.appendChild(child);
+  }
 }
