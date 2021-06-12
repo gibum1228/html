202416b13e3cef593c12331fb75c60adb0cc82c0
Kim, GiBeom <gibum1228@gmail.com>
2021-06-12 21:36:38 +0900

    Vue 중급 프로젝트 - @vue/cli

---
+++ front/router/src/Todo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+      <h3>할 일</h3>
+    </div>
+</template>
+
+<script>
+export default { 
+    name: "Todo" 
+};
+</script>
---
+++ front/vue_app1/vue.config.js
@@ -0,0 +1,6 @@
+module.export= {
+    publicPath: './',
+    configureWebpack: {
+        devtool: 'source-map'
+    }
+}
